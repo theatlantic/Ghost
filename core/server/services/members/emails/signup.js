e44999fcfbaa72594246f5a557da4ce3f0e321cc
Andrew Nikolaenko <andrew@contextly.com>
2021-10-22 11:50:08 +0300

    Add Atlantic logo for emails

--- core/server/services/members/emails/signup.js
+++ core/server/services/members/emails/signup.js
@@ -144,7 +144,7 @@ module.exports = ({siteTitle, email, url, accentColor = '#15212A', siteDomain, s
                 <td class="wrapper" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; font-size: 14px; vertical-align: top; box-sizing: border-box;">
                   <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                     <tr>
-                        <td style="text-align: center; padding-bottom: 40px;">
+                        <td style="text-align: center; padding-bottom: 40px; padding-ещз: 32px;">
                             <img src="https://newsletters.theatlantic.com/stlawrenceriver/ghost/assets/img/email/atlantic-logo.svg" border="0" width="129" height="20" title="Atlantic" alt="Atlantic" />
                         </td>
                     </tr>
