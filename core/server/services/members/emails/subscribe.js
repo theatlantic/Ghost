3a9ab9602296da4ae706116cddb63d1b020655e8
Andrew Nikolaenko <andrew@contextly.com>
2021-12-13 14:34:49 +0200

    Email template updates

--- core/server/services/members/emails/subscribe.js
+++ core/server/services/members/emails/subscribe.js
@@ -182,17 +182,7 @@ module.exports = ({siteTitle, email, url, accentColor = '#15212A', siteDomain, s
                         <td class="wrapper" align="center">
                             <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding-bottom: 20px;">
                                 <tr>
-                                    <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;text-transform: uppercase; color: #5E6A74;padding-top: 5px;">Share The Atlantic Newsletter:</td>
-                                </tr>
-                                <tr>
-                                    <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;color: #5E6A74; padding-bottom: 13px;padding-top: 16px;padding-bottom: 16px;">
-                                        <a href="https://www.facebook.com/TheAtlantic/"><img src="https://newsletters.theatlantic.com/peacefield/ghost/assets/img/email/icon-facebook.png" border="0" width="16" height="16" style="padding-left: 8px;padding-right: 8px;" alt="Facebook"></a>
-                                        <a href="https://twitter.com/TheAtlantic"><img src="https://newsletters.theatlantic.com/peacefield/ghost/assets/img/email/icon-twitter.png" border="0" width="16" height="16" style="padding-left: 8px;padding-right: 8px;" alt="Twitter"></a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;color: #5E6A74; padding-bottom: 13px;">600 New Hampshire Avenue NW ∙ Washington, DC 20037</td>
+                                    <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;color: #5E6A74; padding-bottom: 13px; padding-top: 5px;">600 New Hampshire Avenue NW ∙ Washington, DC 20037</td>
                                 </tr>
                                 <tr>
                                     <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;color: #5E6A74; padding-bottom: 13px;">
@@ -204,7 +194,6 @@ module.exports = ({siteTitle, email, url, accentColor = '#15212A', siteDomain, s
                                 <tr>
                                     <td style="font-family: Arial, sans-serif;font-style: normal;font-weight: normal;font-size: 12px;line-height: 150%;text-align: center;color: #5E6A74; padding-bottom: 13px;">
                                         <a style="color: #5E6A74" href="https://www.theatlantic.com/newsletters/">See all of our newsletters</a>  |
-                                        <a style="color: #5E6A74" href="%unsubscribe_url%">Unsubscribe</a>  |
                                         <a style="color: #5E6A74" href="https://www.theatlantic.com/privacy-policy/">Privacy Policy</a>  |
                                         <a style="color: #5E6A74" href="https://support.theatlantic.com/hc/en-us/requests/new">Customer Care</a>
                                     </td>
@@ -212,7 +201,6 @@ module.exports = ({siteTitle, email, url, accentColor = '#15212A', siteDomain, s
                             </table>
                         </td>
                     </tr>
-
 
                     <!-- END FOOTER -->
 
